20732beda6b77c375703ff967fae41b7cac55bb0
Cole Smith <uniquecolesmith@gmail.com>
2016-07-11 19:46:50 +0800

    + update user schema: add pre-save and instance method comparePassword

--- app/schemas/user.js
+++ app/schemas/user.js
@@ -7,7 +7,7 @@
  ****************************************************/
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-
+var bcrypt = require('bcrypt');
 
 var UserSchema = module.exports = new Schema({
     username: {
@@ -38,3 +38,38 @@ var UserSchema = module.exports = new Schema({
         }
     },
 }, {timestamps: true});
+
+
+UserSchema.pre('save', function (next) {
+    if (this.isModified('password') || this.isNew) {
+        var user = this;
+        bcrypt.genSalt(SALT_WORK_FACTORY, function (err, salt) {
+            if (err) {
+                return next(err);
+            }
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        return next();
+    }
+});
+
+
+UserSchema.methods = {
+    comparePassword: function (_password, cb) {
+        bcrypt.compare(_password, this.password, function (err, isMatch) {
+            if (err) return cb(err);
+
+            cb(null, isMatch);
+        });
+    }
+};
+
+
+
