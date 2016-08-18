7989563e8f35320f0fc1493dfe05e2fa0e58bcd8
eason <uniquecolesmith@gmail.com>
2016-08-18 16:49:28 +0800

    + update store with show user info

--- public/js/admin.js
+++ public/js/admin.js
@@ -94,6 +94,30 @@ $(function () {
 
 
 $(function () {
+    $(".store table .retrieve").click(function (e) {
+        $.ajax({
+            url: prefix + '/user/' + $(this).data('id'),
+            type: 'get',
+            crossDomain: true,
+            // dataType: 'jsonp',
+            // jsonp: 'callback',
+        })
+        .done(function (user) {
+           $("#retrieveModal table tbody").html(
+            `
+            <tr><td>昵&emsp;称</td><td>${user.nickname}</td></tr>
+            <tr><td>用户名</td><td>${user.username}</td></tr>
+            <tr><td>OpenID</td><td>${user.openid}</td></tr>
+            <tr><td>联&emsp;系</td><td>${user.phone}</td></tr>
+            <tr><td>创建时间</td><td>${user.createdAt}</td></tr>
+            <tr><td>更新时间</td><td>${user.updatedAt}</td></tr>
+            `
+            );
+
+            $("#retrieveModal").modal("show");
+        });
+    });
+
     $('.store .btn.update').click(function (e) {
         $.ajax({
             url: prefix + '/store/' + $(this).data('id'),
